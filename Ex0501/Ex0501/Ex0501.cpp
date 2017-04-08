999acf455c3b04ade2f4d34bf58e2c7ff661a8aa
VBTCSE <jhinckley@vbisd.org>
2017-04-08 17:38:59 -0400

    Created initial code for program.

---
+++ Ex0501/Ex0501/Ex0501.cpp
@@ -0,0 +1,13 @@
+// Ex0501.cpp : Defines the entry point for the console application.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << "Program Ex0501" << endl;
+
+	return 0;
+}
+
